09dfa9bd84dfd44bf34ffc5166045fae36cbb135
johnpleung <johnpleung@hotmail.com>
2019-12-04 22:35:38 -0800

    removing vestige of Graphene

--- outbreak/schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
-import outbreaks.schema
-
-class Query(outbreaks.schema.Query, graphene.ObjectType):
-    # This class will inherit from multiple Queries
-    # as we begin to add more apps to our project
-    pass
-
-schema = graphene.Schema(query=Query)
